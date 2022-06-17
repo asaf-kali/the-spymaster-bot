afb66f1f117d0197a1aaa1b973f7b00ce1320efe
Asaf Kali <akali93@gmail.com>
2022-06-18 01:01:35 +0300

    Enable sentry monitoring.

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -1,10 +1,11 @@
 import json
 
+import sentry_sdk
 from the_spymaster_util import get_logger
 
 from bot.config import get_config
 from bot.the_spymaster_bot import TheSpymasterBot
-from main import configure_logging
+from main import configure_logging, configure_sentry
 
 configure_logging()
 log = get_logger(__name__)
@@ -12,6 +13,7 @@ log = get_logger(__name__)
 log.info("Bootstrap starting...")
 config = get_config()
 configure_logging(config=config)
+configure_sentry(config=config)
 bot = TheSpymasterBot(telegram_token=config.telegram_token, base_backend=config.base_backend_url)
 log.info("Bootstrap complete.")
 
@@ -29,8 +31,9 @@ def handle(event: dict, context=None):
             log.warning("Error decoding JSON")
             return create_response(400, data={"message": "Error decoding JSON", "error": str(e)})
         bot.process_update(update_data)
-    except:  # noqa
+    except Exception as e:
         log.exception("Error handling event")
+        sentry_sdk.capture_exception(e)
         return create_response(500, data={"message": "Error handling event"})
 
 
@@ -40,22 +43,14 @@ def create_response(status_code: int, data: dict):
 
 def example_event():
     telegram_update = {
-        "update_id": 617241324,
+        "update_id": 617241338,
         "message": {
-            "message_id": 2577,
-            "from": {
-                "id": 1362351931,
-                "is_bot": False,
-                "first_name": "\\ud835\\udca6\\ud835\\udcb6\\ud835\\udcc1\\ud835\\udcbe",
-                "language_code": "en",
-            },
-            "chat": {
-                "id": 1362351931,
-                "first_name": "\\ud835\\udca6\\ud835\\udcb6\\ud835\\udcc1\\ud835\\udcbe",
-                "type": "private",
-            },
-            "date": 1655397228,
-            "text": "asdf",
+            "message_id": 2611,
+            "from": {"id": 1362351931, "is_bot": False, "first_name": "𝒦𝒶𝓁𝒾", "language_code": "en"},
+            "chat": {"id": 1362351931, "first_name": "𝒦𝒶𝓁𝒾", "type": "private"},
+            "date": 1655500031,
+            "text": "/start",
+            "entities": [{"offset": 0, "length": 6, "type": "bot_command"}],
         },
     }
     event = {"body": json.dumps(telegram_update)}
