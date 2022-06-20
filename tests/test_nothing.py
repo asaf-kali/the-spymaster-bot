6fa0adcbaec9917ce3f3bac75c761cbfc3da10d5
Asaf Kali <akali93@gmail.com>
2022-06-20 21:12:23 +0300

    Better tests setup.

--- tests/test_nothing.py
+++ tests/test_nothing.py
@@ -1,2 +1,11 @@
-def test_nothing():
-    assert True
+import logging
+
+from bot.config import Config, get_config
+
+log = logging.getLogger(__name__)
+
+
+def test_config_loads():
+    config: Config = get_config()
+    config.load()
+    log.info(f"Config loaded, environment: {config.env_verbose_name}")
