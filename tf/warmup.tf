527e28110f1c41473d2b82123c885d77694d3e91
Asaf Kali <akali93@gmail.com>
2023-01-07 23:38:04 +0200

    Re-enable warmups.

--- tf/warmup.tf
+++ tf/warmup.tf
@@ -1,22 +1,22 @@
-## Lambda warmup event
-#
-#resource "aws_cloudwatch_event_rule" "lambda_warmup_rule" {
-#  schedule_expression = "rate(3 minutes)"
-#}
-#
-#resource "aws_cloudwatch_event_target" "lambda_warmup_target" {
-#  rule  = aws_cloudwatch_event_rule.lambda_warmup_rule.name
-#  arn   = aws_lambda_function.service_lambda.arn
-#  input = <<EOF
-#{
-#  "body": "{ \"action\": \"warmup\" }"
-#}
-#EOF
-#}
-#
-#resource "aws_lambda_permission" "lambda_warmup_permission" {
-#  action        = "lambda:InvokeFunction"
-#  principal     = "events.amazonaws.com"
-#  source_arn    = aws_cloudwatch_event_rule.lambda_warmup_rule.arn
-#  function_name = aws_lambda_function.service_lambda.function_name
-#}
+# Lambda warmup event
+
+resource "aws_cloudwatch_event_rule" "lambda_warmup_rule" {
+  schedule_expression = "rate(3 minutes)"
+}
+
+resource "aws_cloudwatch_event_target" "lambda_warmup_target" {
+  rule  = aws_cloudwatch_event_rule.lambda_warmup_rule.name
+  arn   = aws_lambda_function.service_lambda.arn
+  input = <<EOF
+{
+  "body": "{ \"action\": \"warmup\" }"
+}
+EOF
+}
+
+resource "aws_lambda_permission" "lambda_warmup_permission" {
+  action        = "lambda:InvokeFunction"
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.lambda_warmup_rule.arn
+  function_name = aws_lambda_function.service_lambda.function_name
+}
