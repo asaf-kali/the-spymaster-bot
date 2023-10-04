8f101c0c39ebda58236d93aad506db97d1f9a292
Asaf Kali <akali93@gmail.com>
2023-10-05 00:23:46 +0300

    Small refactor.

--- scripts/build_layer.sh
+++ scripts/build_layer.sh
@@ -6,5 +6,5 @@ install_dependencies_cmd="pip install -r requirements.lock -t ${export_folder}"
 docker_cmd="${update_pip_cmd}; ${install_dependencies_cmd}; exit"
 
 sudo rm -rf "$export_folder"
-make export
+make lock-export
 docker run -v "$PWD":/var/task "$image_name" /bin/sh -c "$docker_cmd"
