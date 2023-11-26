ff5b6873c104e3aed718e190a8faf3da2fef3128
Asaf Kali <akali93@gmail.com>
2023-11-26 14:13:18 +0200

    Fix load models.

--- src/bot/handlers/load_models.py
+++ src/bot/handlers/load_models.py
@@ -1,4 +1,4 @@
-from the_spymaster_util.measure_time import MeasureTime
+import time
 
 from bot.handlers.base import EventHandler
 
@@ -6,6 +6,8 @@ from bot.handlers.base import EventHandler
 class LoadModelsHandler(EventHandler):
     def handle(self):
         self.send_text("Sending load models request...")
-        with MeasureTime() as mt:  # pylint: disable=invalid-name
-            response = self.bot.send_load_models_request()
-        self.send_markdown(f"Loaded `{response.success_count}` models in `{mt.delta}` seconds.")
+        start_ts = self.update.message.date.timestamp()
+        response = self.bot.send_load_models_request()
+        end_ts = time.time()
+        delta = round(end_ts - start_ts, 3)
+        self.send_markdown(f"Loaded `{response.success_count}` models in `{delta}` seconds.")
