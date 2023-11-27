d653193dcf1b9c91d72bd57ae2dd023573feb675
Asaf Kali <akali93@gmail.com>
2023-11-27 02:00:22 +0200

    Auto commit

--- src/bot/handlers/load_models.py
+++ src/bot/handlers/load_models.py
@@ -8,7 +8,7 @@ class LoadModelsHandler(EventHandler):
         receive_ts = time.time()
         sent_ts = self.update.message.date.timestamp()
         receive_delta = round(receive_ts - sent_ts, 3)
-        self.send_text(f"Sending load models request (receive took `{receive_delta}` seconds)...")
+        self.send_markdown(f"Sending load models request (receive took `{receive_delta}` seconds)...")
         load_start_ts = time.time()
         response = self.bot.send_load_models_request()
         load_end_ts = time.time()
