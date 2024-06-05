bad79f5bf144e69eb5bb6043c4c5de74bd1fa26a
Asaf Kali <akali93@gmail.com>
2024-06-05 14:42:50 +0300

    🛣️ Update DNS name.

--- tf/main.tf
+++ tf/main.tf
@@ -42,21 +42,22 @@ locals {
     "default" = "dev"
     "prod"    = "prod"
   }
-  env               = local.env_map[terraform.workspace]
-  is_prod           = local.env == "prod"
+  env             = local.env_map[terraform.workspace]
+  is_prod = local.env == "prod"
   # Base
-  project_name      = "the-spymaster-bot"
-  service_name      = "${local.project_name}-${local.env}"
-  aws_account_id    = data.aws_caller_identity.current.account_id
+  project_name    = "the-spymaster-bot"
+  service_name    = "${local.project_name}-${local.env}"
+  aws_account_id = data.aws_caller_identity.current.account_id
   # Paths
-  tf_root           = abspath(path.module)
-  project_root      = abspath("${path.module}/../")
-  layer_src_root    = "${local.project_root}/.deployment/layer-dependencies"
-  lambda_src_root   = "${local.project_root}/src/"
+  tf_root = abspath(path.module)
+  project_root = abspath("${path.module}/../")
+  layer_src_root  = "${local.project_root}/.deployment/layer-dependencies"
+  lambda_src_root = "${local.project_root}/src/"
   # Domain
-  base_app_domain   = "the-spymaster.xyz"
-  hosted_zone_id    = "Z0770508EK6R7V32364I"
-  certificate_arn   = "arn:aws:acm:us-east-1:${local.aws_account_id}:certificate/fc0faea8-e891-438a-a779-4013ee38755f"
+  base_app_domain = "303707.xyz"
+  hosted_zone_id  = "Z0883104W79ZV11TF62Q"
+  certificate_id  = "7c013d7d-0ff5-45df-97a8-c34019e44bdb"
+  certificate_arn = "arn:aws:acm:us-east-1:${local.aws_account_id}:certificate/${local.certificate_id}"
   domain_suffix_map = {
     "dev"     = "dev."
     "staging" = ""
@@ -64,7 +65,7 @@ locals {
   }
   domain_suffix      = local.domain_suffix_map[local.env]
   bot_webhook_domain = "telegram.${local.domain_suffix}${local.base_app_domain}"
-  bot_endpoint_url   = "${aws_apigatewayv2_api.api_gateway.api_endpoint}/${aws_apigatewayv2_stage.api_stage.name}/"
+  bot_endpoint_url = "${aws_apigatewayv2_api.api_gateway.api_endpoint}/${aws_apigatewayv2_stage.api_stage.name}/"
   # Encryption
   default_key_arn    = "arn:aws:kms:us-east-1:${local.aws_account_id}:key/0b9c713c-1c4b-43ad-84df-1f62117838f0"
   # Config
